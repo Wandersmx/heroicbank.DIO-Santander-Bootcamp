b04695e19877a319f18ddebfadd9ff904bf96a7e
Wandersmx <wadson.luiz@gmail.com>
2022-06-06 16:12:25 -0300

    updated to apply the private modifier to set function of cpf property

--- src/main/kotlin/heroicbank/digitalinovation/heroicbank/Pessoa.kt
+++ src/main/kotlin/heroicbank/digitalinovation/heroicbank/Pessoa.kt
@@ -2,7 +2,10 @@ package heroicbank.digitalinovation.heroicbank
 
 class Pessoa {
     var nome: String = "Altria Pendragon"
+
     var cpf: String = "123.456.789-10"
+        //modificador para deixar o set privado afim de nao deixar a informação pesquisável/editavel
+    private set
 }
 
 //Função apenas para teste afim de retornar os objetos instanciados na classe
@@ -11,4 +14,5 @@ fun main(){
 
     println(Altria.nome)
     println(Altria.cpf)
+    println(Altria)
 }
