5888d04a2af30b103754809ea3116ff5216d1c0f
Wandersmx <wadson.luiz@gmail.com>
2022-06-06 16:26:25 -0300

    updated to talk about class members

--- src/main/kotlin/heroicbank/digitalinovation/heroicbank/Pessoa.kt
+++ src/main/kotlin/heroicbank/digitalinovation/heroicbank/Pessoa.kt
@@ -1,18 +1,22 @@
 package heroicbank.digitalinovation.heroicbank
 
 class Pessoa {
-    var nome: String = "Altria Pendragon"
 
+    //Cada dado em uma classe são membros.
+    var nome: String = "Altria Pendragon"
     var cpf: String = "123.456.789-10"
         //modificador para deixar o set privado afim de nao deixar a informação pesquisável/editavel.
     private set
+
+    //Quando é chamada a função pessoa apos os dados serem inicializados o constructor é iniciilizado.
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 //Função apenas para teste afim de retornar os objetos instanciados na classe
 fun main(){
     val Altria = Pessoa()
+    println(Altria.pessoaInfo())
 
-    println(Altria.nome)
-    println(Altria.cpf)
-    println(Altria)
 }
